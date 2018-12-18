5663b7343fe67d3d37ab78370c88586514e495fb
cao hoang long <mrlong1206@gmail.com>
2018-12-19 01:39:54 +0700

    fix

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '/../client/build', 'index.html'));
 });
 
 module.exports = router;
